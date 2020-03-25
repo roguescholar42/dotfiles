de097f8e25a39a443b35da71be4b68392e4de22e
Josh <roguescholar42@gmail.com>
2020-03-24 23:29:42 -0400

    Added Oh My Zsh installer

--- setup.sh
+++ setup.sh
@@ -4,8 +4,11 @@
 # Script for setting up enviroment to backup and restore.  #
 ############################################################
 
-#Determines location of the file and places it in the BASEDIR variable.
+# Determines location of the file and places it in the BASEDIR variable.
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-#symlink mackup config to this repo.
+# symlink mackup config to this repo.
 ln -s ${BASEDIR}/.mackup.cfg ~/.mackup.cfg
+
+# Install oh my zsh.
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
