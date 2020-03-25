625b22d32e9f813e885aef5da8cefa979d9bcfd8
Josh <roguescholar42@gmail.com>
2020-03-24 23:16:44 -0400

    Added reload script

---
+++ reload.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#######################################################
+# Script for importing apps and config files          #
+#######################################################
+
+brew update
+brew bundle 
+brew cask upgrade
