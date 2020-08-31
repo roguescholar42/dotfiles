4a8a78def9ef48463d2937d3135a0a2cda3f0c7a
Josh Evans <roguescholar42@gmail.com>
2020-08-31 01:19:59 -0400

    Updated hyper plugins

--- backup/.hyper.js
+++ backup/.hyper.js
@@ -104,7 +104,11 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-quit"],
+  plugins: [
+    "hyper-quit",
+    "hyperline",
+    "hyper-hide-title",
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
