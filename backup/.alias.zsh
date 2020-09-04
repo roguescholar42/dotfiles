87408795e56c5da3fe95d3fabacf1ba8837bf731
Josh Evans <roguescholar42@gmail.com>
2020-09-04 15:45:06 -0400

    Updated count alias

--- backup/.alias.zsh
+++ backup/.alias.zsh
@@ -1,3 +1,4 @@
 # Defines custom aliases.
 
 alias checkconfigs='code ~/.dotfiles' # Opens Dotfiles folder in VSCode. 
+alias count='cd ~/documents/github/roguescholar42.github.io/_posts && git pull && tree' # Shows post count on blog.
