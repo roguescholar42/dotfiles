e863c14ff180d468e07c78ac47c2cfccbe9e3e3a
Josh Evans <roguescholar42@gmail.com>
2020-09-20 23:46:44 -0400

    Added First run script

---
+++ scripts/first.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+######################################################################
+# First run script for setting up a computer to work with dotfiles.  #
+######################################################################
+
+echo "test passed."
