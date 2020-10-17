d3048e090fe917a28dfaf6ef427660f051a65ee3
Josh Evans <roguescholar42@gmail.com>
2020-10-17 01:32:00 -0400

    Added aliases for SSH

--- backup/.alias.zsh
+++ backup/.alias.zsh
@@ -3,3 +3,6 @@
 alias checkconfigs='code ~/.dotfiles' # Opens Dotfiles folder in VSCode. 
 alias count='cd ~/documents/github/roguescholar42.github.io/_posts && git pull && tree' # Shows post count on blog.
 alias reload='exec zsh' # Reloads zsh
+
+# SSH aliases
+alias rpi='ssh pi@raspberrypi.local'
