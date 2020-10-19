31dde92df90fedce5bed2fc40e221d2e2f9454e2
Josh Evans <roguescholar42@gmail.com>
2020-10-18 20:21:01 -0400

    Dotfiles Updated.

--- backup/.alias.zsh
+++ backup/.alias.zsh
@@ -3,6 +3,7 @@
 alias checkconfigs='code ~/.dotfiles' # Opens Dotfiles folder in VSCode. 
 alias count='cd ~/documents/github/roguescholar42.github.io/_posts && git pull && tree' # Shows post count on blog.
 alias reload='exec zsh' # Reloads zsh
+alias ls='ls -G' # Forces output of LS to be colorized.
 
 # SSH aliases
 alias rpi='ssh pi@raspberrypi.local'
