280ea45bbbdf406c9b5a70d33f7fbe39eacd3824
Josh Evans <roguescholar42@gmail.com>
2020-10-29 19:06:34 -0400

    Dotfiles Updated.

--- backup/.alias.zsh
+++ backup/.alias.zsh
@@ -1,6 +1,7 @@
 # Defines custom aliases.
 
 alias checkconfigs='code ~/.dotfiles' # Opens Dotfiles folder in VSCode. 
+alias checkscripts='code ~/.scripts' # Opens Scripts folder in VSCode.
 alias count='cd ~/documents/github/roguescholar42.github.io/_posts && git pull && tree' # Shows post count on blog.
 alias reload='exec zsh' # Reloads zsh
 alias ls='ls -G' # Forces output of LS to be colorized.
