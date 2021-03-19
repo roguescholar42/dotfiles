a9e7b469f8fad38d8f90f58be5706eebc8022d17
Josh Evans <roguescholar42@gmail.com>
2021-03-19 03:51:38 -0400

    Cleaned up a line in alias

--- dot_alias.zsh
+++ dot_alias.zsh
@@ -1,5 +1,4 @@
 # Defines custom aliases.
-
 alias checkconfigs='code ~/.local/share/chezmoi' # Opens Dotfiles folder in VSCode. 
 alias count='cd ~/documents/github/roguescholar42.github.io/_posts && git pull && tree' # Shows post count on blog.
 alias reload='exec zsh' # Reloads zsh
