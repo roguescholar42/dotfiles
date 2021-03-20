df3d4b1c1a3f9808e969601052ce1368cdbfd8ec
Josh Evans <roguescholar42@gmail.com>
2021-03-19 22:40:56 -0400

    Finished Bootstraping script

--- run_once_bootstrap.sh
+++ run_once_bootstrap.sh
@@ -7,6 +7,7 @@
 # Loads kernel into variable.
 KERNEL=$(uname)
 
+# Checks if MacOS or Linux and performs installs based on platform.
 if [ $KERNEL == "Darwin" ]; then
     # Checks if brewfile exists. If so, installs apps.
     if [ -f "$HOME/.Brewfile" ]; then
@@ -14,6 +15,31 @@ if [ $KERNEL == "Darwin" ]; then
         brew bundle --global
     fi
 
-elif [ $KERNEL == ""]; then 
+elif [ $KERNEL == "Linux" ]; then 
+    # Updates and installs apps using apt-get.
+    echo "Updating apt and preinstalled packages."
+    sudo apt-get update
+    sudo apt-get upgrade -y
+
+    echo "Installing apps."
+    sudo apt-get install zsh -y
+    sudo apt-get install neofetch -y
+
+    # Checks if Antibody files doesn't exist. If not, it installs it.
+    if [ ! -e "/usr/local/bin/antibody" ]; then
+        echo "Installing Antibody."
+        curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
+    fi
+    
 fi
 
+# Checks if Zsh is default shell.
+if [ $SHELL == "/bin/zsh" ]; then
+    # Reloads zsh if default. 
+    echo "Reloading shell."
+    exec zsh
+else
+    # Sets Zsh as the main shell. 
+    echo "Setting Zsh as default."
+    chsh -s /bin/zsh 
+fi
