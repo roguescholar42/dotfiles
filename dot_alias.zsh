1a2b9a92613656d5b3ff8eebca315a929a25ac44
Josh Evans <roguescholar42@gmail.com>
2021-03-20 23:52:36 -0400

    Added config for nano

--- dot_alias.zsh
+++ dot_alias.zsh
@@ -3,7 +3,8 @@ alias checkconfigs='code ~/.local/share/chezmoi' # Opens Dotfiles folder in VSCo
 alias count='cd ~/documents/github/roguescholar42.github.io/_posts && git pull && tree' # Shows post count on blog.
 alias reload='exec zsh' # Reloads zsh
 alias ls='ls -G' # Forces output of LS to be colorized.
-alias goodmorningsoda='code ~/documents/github/roguescholar42.github.io' #Opens blog files in VSCode.
+alias goodmorningsoda='code ~/documents/github/roguescholar42.github.io' # Opens blog files in VSCode.
+alias nano='/usr/local/bin/nano' # Sets nano command to version downloaded by Brew.
 
 # SSH aliases
 alias rpi='ssh pi@raspberrypi.local'
