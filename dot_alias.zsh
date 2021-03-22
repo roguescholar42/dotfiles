9f87caea81dfa5e52193d5298afbac1bb30e8001
Josh Evans <roguescholar42@gmail.com>
2021-03-21 22:07:17 -0400

    Moved nano alias to custom template

--- dot_alias.zsh
+++ dot_alias.zsh
@@ -4,7 +4,6 @@ alias count='cd ~/documents/github/roguescholar42.github.io/_posts && git pull &
 alias reload='exec zsh' # Reloads zsh
 alias ls='ls -G' # Forces output of LS to be colorized.
 alias goodmorningsoda='code ~/documents/github/roguescholar42.github.io' # Opens blog files in VSCode.
-alias nano='/usr/local/bin/nano' # Sets nano command to version downloaded by Brew.
 
 # SSH aliases
 alias rpi='ssh pi@raspberrypi.local'
